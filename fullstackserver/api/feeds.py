13a15f1d5eb92ad2a778b75db1882dec7ce08790
aviaryan <avi.aryan123@gmail.com>
2016-03-13 12:16:46 +0530

    refactored code

--- fullstackserver/api/feeds.py
+++ fullstackserver/api/feeds.py
@@ -6,38 +6,24 @@ from .models import *
 from .integrations import twitter, tumblr, instagram
 
 def addLinksForNewSub(uSub):
-    sp = uSub.searchParam
-    # twitter
-    links = twitter.getLinks(sp)
-    names = twitter.getNames(links)
-    count = 0
-    for i in range(len(links)):
-        if names[i] == 'search':
-            continue
-        existingLinks = Link.objects.filter(name=names[i], network='twitter')
-        if len(existingLinks) > 0:
-            link = existingLinks[0]
-        else:
-            link = Link(url = links[i], network='twitter', name=names[i])
-            link.save()
-        sl = SubscriptionLink(userSub=uSub, link=link)
-        sl.save()
-        count+=1
-        if count == 3:
-            break
+    getLinksForNetwork(uSub, twitter, 'twitter')
+    getLinksForNetwork(uSub, instagram, 'instagram')
+    getLinksForNetwork(uSub, tumblr, 'tumblr')
 
-    # instagram
-    links = instagram.getLinks(sp)
-    names = instagram.getNames(links)
+
+def getLinksForNetwork(uSub, networkClass, networkStr):
+    sp = uSub.searchParam
+    links = networkClass.getLinks(sp)
+    names = networkClass.getNames(links)
     count = 0
     for i in range(len(links)):
         if names[i] == 'search':
             continue
-        existingLinks = Link.objects.filter(name=names[i], network='instagram')
+        existingLinks = Link.objects.filter(name=names[i], network=networkStr)
         if len(existingLinks) > 0:
             link = existingLinks[0]
         else:
-            link = Link(url = links[i], network='instagram', name=names[i])
+            link = Link(url = links[i], network=networkStr, name=names[i])
             link.save()
         sl = SubscriptionLink(userSub=uSub, link=link)
         sl.save()

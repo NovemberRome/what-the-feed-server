22bc0bdb30ee137c80b0125408d5d8b87f7c02aa
aviaryan <avi.aryan123@gmail.com>
2016-03-14 10:42:03 +0530

    update README

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Server for FullStack's Hackathon project
+# What The Feed server
 
-Built in Django
+What The Feed is a social media aggregration platform that helps you follow related social media accounts at once. Suppose you are interested in some topic, say "Indian Finance", WTF will automatically find the leading social media accounts for that topic, follow their posts and provide you with an aggregrated feed.
+
+
+## Hackathon
+
+This was team FullStack's project for IIITV Hackathon 2016. Team members were - 
+* Avi Aryan @aviaryan
+* Pratyush Singh @singh-pratyush96
+* Raju Koushik @RajuKoushik
+* Vaibhav
+* Vishal
+
+Social networks that were integrated at this point are Twitter, Instagram and Tumblr.
+
+
+## Future Plans
+
+Let's see.
--- _scripts/feeds.py
+++ _scripts/feeds.py
@@ -4,15 +4,15 @@ url = 'http://10.100.2.81:5123/api/feed/'
 #url = 'http://whatthefeed.herokuapp.com/api/feed/'
 
 def addsubs():
-    r = requests.post(url + 'addsubscription', data = {'id': 2, 'password': 'a', 'searchparam': 'Artificial Intelligence'})
+    r = requests.post(url + 'addsubscription', data = {'id': 1, 'password': 'something', 'searchparam': 'India'})
     print(r.content)
 
 def delsubs():
-    r = requests.post(url + 'deletesubscription', data = {'id': 1, 'password': 'something', 'subsid': 2})
+    r = requests.post(url + 'deletesubscription', data = {'id': 2, 'password': 'a', 'subsid': 5})
     print(r.content)
 
 def getfeed():
-    r = requests.post(url + 'getfeed', data = {'id': 1, 'password': 'something', 'subsid': 1})
+    r = requests.post(url + 'getfeed', data = {'id': 1, 'password': 'something', 'subsid': 17})
     print(r.content)
 
 def getmainfeed():
@@ -20,7 +20,7 @@ def getmainfeed():
     print(r.content)
 
 if __name__ == '__main__':
-    addsubs()
+    #addsubs()
     #delsubs()
-    #getfeed()
+    getfeed()
     #getmainfeed()
